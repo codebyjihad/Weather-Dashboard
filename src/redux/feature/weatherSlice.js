16d5353624c958430dc2c579435ecfd7b37f558a
jihad <codebyjihad@gmail.com>
2026-02-20 12:53:26 +0600

    feat: set weather functionaliy

--- src/redux/feature/weatherSlice.js
+++ src/redux/feature/weatherSlice.js
@@ -1,49 +1,62 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    weatherData:[],
-    loading:false,
-    error:null
-}
+  weatherData: null,   
+  loading: false,
+  error: null,
+};
 
-// fetch wathers data from weather api
 
-export const fetchWeatherData = createAsyncThunk('weather/fetchWeatherData', async(cityName) => {
-    const apikey = "71a9bd4b7ce445493c9651da1262d9b2" ;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apikey}`)
-     
-    const data = await response.json()
+export const fetchWeatherData = createAsyncThunk(
+  "weather/fetchWeatherData",
+  async (cityName, thunkAPI) => {
+    try {
+      const apikey = "71a9bd4b7ce445493c9651da1262d9b2";
 
-    return data
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apikey}`
+      );
 
-})
+      const data = await response.json();
 
+      
+      if (!response.ok) {
+        return thunkAPI.rejectWithValue(data.message);
+      }
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Network error");
+    }
+  }
+);
 
 const weathers = createSlice({
-    name:'weather',
-    initialState:initialState,
-    reducers:{
-        clearWeatherData:(state) => {
-            state.weatherData = []
-        }
+  name: "weather",
+  initialState,
+  reducers: {
+    clearWeatherData: (state) => {
+      state.weatherData = null;
+      state.error = null;
+      state.loading = false;
     },
-    extraReducers:(builder) => {
-        builder
-        .addCase(fetchWeatherData.pending, (state) => {
-            state.loading = true,
-            state.error = null
-        })
-        .addCase(fetchWeatherData.fulfilled, (state , action) => {
-            state.loading = false,
-            state.weatherData.push(action.payload)
-        })
-        .addCase(fetchWeatherData.rejected , (state , action) =>{
-            state.loading = false,
-            state.error = action.error.message
-        })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeatherData.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchWeatherData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.weatherData = action.payload;  
+      })
+      .addCase(fetchWeatherData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload; 
+      });
+  },
+});
 
-export const { clearWeatherData} = weathers.actions
-
-export default weathers.reducer
+export const { clearWeatherData } = weathers.actions;
+export default weathers.reducer;
